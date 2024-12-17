13951d154ddf88ace0d8a87ddebace152c10b610
hermine serges nyaleu ngassa <herminangassa@gmail.com>
2024-12-17 15:42:45 +0100

    ceasar/vigenere

---
+++ main.py
@@ -0,0 +1,8 @@
+#creation de la liste 
+fruits = ["mangue", "tomates", "bananes"]
+#j'affiche les elements de la liste 
+print(fruits)
+#modification de la liste pour ajouter orange
+fruits = fruits + ["oranges"]
+#j'affiche la liste modifiée
+print(fruits)
